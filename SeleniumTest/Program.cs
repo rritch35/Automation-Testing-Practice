a56334bd3228dc4b05ebe86462c529ca5b716c3a
rritch35 <rritch35@gmail.com>
2025-02-12 11:22:00 -0500

    Automation test for google webpage title

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            IWebDriver driver = new ChromeDriver();//creating chrome driver to open webpage called driver
+
+            
+            driver.Navigate().GoToUrl("https://www.google.com");//use chrome driver to open google
+
+           
+            string pageTitle = driver.Title;//create a string called pageTitle value of the title of the page
+            Console.WriteLine("Page Title: " + pageTitle);//output the title of the page for readiblity
+
+            
+            if (pageTitle.Contains("Google"))//if pageTitle is google
+            {
+                Console.WriteLine("Test Passed!");//test passed
+            }
+            else
+            {
+                Console.WriteLine("Test Failed!");
+            }
+
+    
+            driver.Quit();
+        }
+    }
+}
